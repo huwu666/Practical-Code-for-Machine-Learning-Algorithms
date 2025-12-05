a5b561d6896b3d0b39ac4a08d221f10992418bcd
analy <3064014960@qq.com>
2025-12-05 22:19:06 +0800

    1
    1

---
+++ 第四章/bayes.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+from numpy import *
+
+def loadDataSet():
+
+    postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                   ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                   ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                   ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                   ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    
+    classVal = [0, 1, 0, 1, 0, 1] #1代表侮辱性文字,0代表正常言论
+    return postingList, classVal
+
+def createVocabList(dataSet):
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+def setOfWords2Vec(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else: 
+            print ("the word: %s is not in my Vocabulary!" % word)
+    return returnVec
+    
+def _trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+
+    p0Num = zeros(numWords)
+    p1Num = zeros(numWords)
+
+    p0Denom = 0.0
+    p1Denom = 0.0
+    for i in range(numTrainDocs):
+
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+
+    p1Vect = p1Num / p1Denom
+    p0Vect = p0Num / p0Denom
+
+    return p0Vect, p1Vect, pAbusive
+
+
+listPosts, listClasses = loadDataSet()
+
+myVocabList = createVocabList(listPosts)
+print("cihuibiaodaxiao", len(myVocabList))
+print("cihuibiaoqian10gedanci", myVocabList[:10])
+
+trainMat = []
+for postinDoc in listPosts:
+    trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+
+p0V, p1V, pAb = _trainNB0(trainMat, listClasses)
+print(pAb)
+print(len(p0V))
+print(len(p1V))
