13f591e8274cdc1ab89bfb0ebfe6ee81d4fb8033
analy <3064014960@qq.com>
2025-12-05 17:21:49 +0800

    1
    1

--- 第二章/kNN.py
+++ 第二章/kNN.py
@@ -41,4 +41,26 @@ def file2matrix(filename):
     fr = open(filename)
     numberOfLines = len(fr.readlines())
     returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    fr = open(filename)
+    
+    index = 0
+
+    for line in fr.readlines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+
+        returnMat[index, :] = listFromLine[0:3]
+
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+
+    return returnMat, classLabelVector
+
+def autoNorm(dataSet):
+
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+
+    ranges = maxVals - minVals
     
